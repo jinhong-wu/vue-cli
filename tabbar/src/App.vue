642d47a76038d851d4332749cda0f9c2f6e83f51
jinhong-wu <jinhong212@163.com>
2020-09-23 17:43:16 +0800

    PC：tabbar

---
+++ tabbar/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view />
+    <tab-bar :list="list"></tab-bar>
+  </div>
+</template>
+
+<script>
+import tabbar from "./components/tabbar.vue";
+export default {
+  name: "App",
+  data() {
+    return {
+      list: [
+        { title: '首页' },
+        { title: '商城' },
+        { title: '我的' },
+        ],
+    };
+  },
+  conponents: {
+    tabbar,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ tabbar/src/components/tabbar.vue
@@ -0,0 +1,29 @@
+<template id="tabbar">
+  <div class="footer">
+      <div class="item" v-for="item in list">
+        <slot></slot>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  template: "#tabbar",
+  props: ["list"],
+};
+</script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  display: inline-flex;
+  width: 100%;
+  height: 60px;
+  bottom: 0;
+  left: 0;
+  flex-wrap: wrap;
+}
+.footer .item {
+  flex: 1;
+}
+</style>
